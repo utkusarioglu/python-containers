9769430e176936d021080852c800a5591f4e37f2
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-19 16:11:27 +0300

    Feat(ci): Adopt devcontainer spec
    - Add `.devcontainer/devcontainer.json` to all conda environments.
      The spec file installs github cli and ssh support to the
      devcontainers.
    - Alter `Dockerfile.conda.dev`:
      - Alter comment on line 26 to make it clearer.
      - Alter download url for gists at 33, 37, 41 for them to download
        the latest version of the gists every time. This is done to reduce
        maintenance burden.
      - Add step for copying the devcontainer checker script file to the
        container. This file is used by CI to run inside the newly created
        devcontainer to check whether everything works as expected.
    - Add `devcontainer-check.sh` script to all the environment. This is
      the devcontainer checker mentioned in the previous bullet.
    - Alter `build-and-push.sh`:
      - Set tags for triggering runs
      - Remove branches from triggers
      - Add the workflow file itself to the `changed_files` checker at
        ln 68.
      - Add tag checking to `run_state` step to ensure that the run has a
        tag.
      - Add action `utkusarioglu/devcontainer-build@main` among the steps
        to build a devcontainer that is compliant with the specs.
      - Remove steps that are rendered obsolete by the previous bullet.

---
+++ src/econ/scripts/devcontainer-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+required_packages="gh python vim git"
+
+for exec in $required_packages;
+do
+  if [ -z "$(which $exec)" ];
+  then
+    echo "Error: $exec is not available inside the container"
+    exit 1
+  fi
+done
