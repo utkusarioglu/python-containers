12bb70c1421f766bf19bb07f14f26d4c64169bdb
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-12 17:16:38 +0000

    py New

--- src/home/scripts/bootstrap/python/set-symlinks.sh
+++ src/home/scripts/bootstrap/python/set-symlinks.sh
@@ -3,7 +3,3 @@
 set -eux
 
 ln -sf $(which python3.13) /usr/bin/python
-
-wget -O - https://bootstrap.pypa.io/get-pip.py | python
-
-pip --version
--- src/home/scripts/bootstrap/python/set-venv.sh
+++ src/home/scripts/bootstrap/python/set-venv.sh
@@ -3,9 +3,15 @@
 set -eux
 
 ARGS=(
+  home_abspath
   venv_path
 )
 . ${0%/*}/../linux/parse-args.sh
 
-python -m venv ${venv_path}
-echo "source ${venv_path}/bin/activate" >> ~/.bashrc
+python -m venv --without-pip ${venv_path}
+echo "source ${venv_path}/bin/activate" >> ${home_abspath}/.bashrc
+
+source ${home_abspath}/.bashrc
+
+wget -O - https://bootstrap.pypa.io/get-pip.py | python
+pip --version
