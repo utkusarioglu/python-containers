00aca16587d1c649dededf4af8567c825e29b705
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-12 15:11:35 +0000

    py New

--- src/home/scripts/bootstrap/python/install-requirements.sh
+++ src/home/scripts/bootstrap/python/install-requirements.sh
@@ -6,7 +6,7 @@ ARGS=(
   home_abspath
   venv_path
 )
-. ${0%/*}/parse-args.sh
+. ${0%/*}/../linux/parse-args.sh
 
 source ${venv_path}/bin/activate
 pip install --require-virtualenv -r "${home_abspath}/requirements.txt"
--- src/home/scripts/bootstrap/python/set-venv.sh
+++ src/home/scripts/bootstrap/python/set-venv.sh
@@ -5,7 +5,7 @@ set -eux
 ARGS=(
   venv_path
 )
-. ${0%/*}/parse-args.sh
+. ${0%/*}/../linux/parse-args.sh
 
 python -m venv ${venv_path}
 echo "source ${venv_path}/bin/activate" >> ~/.bashrc
