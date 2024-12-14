c5b662b73cdfdddc9a06117ad226110906c04c68
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-14 15:23:38 +0000

    py New

--- src/home/scripts/bootstrap/conda/install-micromamba.sh
+++ src/home/scripts/bootstrap/conda/install-micromamba.sh
@@ -15,16 +15,17 @@ wget -O - https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/m
 eval "$(./bin/micromamba shell hook -s posix)"
 
 # Linux/bash:
-./bin/micromamba shell init -s bash -r ~/micromamba  # this writes to your .bashrc file
+./bin/micromamba shell init -s bash -r ${home_abspath}/micromamba  # this writes to your .bashrc file
 # sourcing the bashrc file incorporates the changes into the running session.
 # better yet, restart your terminal!
-echo "eval \"\$(micromamba shell hook --shell=bash)\" && micromamba activate $ENVIRONMENT_NAME" >> ~/.bashrc
+echo "eval \"\$(micromamba shell hook --shell=bash)\" && micromamba activate ${ENVIRONMENT_NAME}" >> ${HOME_ABSPATH}/.bashrc
 echo 'alias mm=micromamba' >> "${HOME_ABSPATH}/.bash_aliases"
 
-source ~/.bashrc
+source ${home_abspath}/.bashrc
 
 # micromamba create -n env_name ${environment_name} -c conda-forge
 micromamba --version
+# THIS NEEDS TO WORK
 mm --version
 
 # COPY ${environment_config} /environment.repo.yml
