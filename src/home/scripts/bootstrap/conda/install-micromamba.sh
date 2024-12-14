2ea6c08e1f4b45f450daefac0c7f63d87748bd2f
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-14 15:37:52 +0000

    py New

--- src/home/scripts/bootstrap/conda/install-micromamba.sh
+++ src/home/scripts/bootstrap/conda/install-micromamba.sh
@@ -9,29 +9,21 @@ ARGS=(
 )
 . ${0%/*}/../linux/parse-args.sh
 
-micromamba_relpath=bin/micromamba
 micromamba_abspath="${home_abspath}/${micromamba_relpath}"
 
-wget -O - https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj "./${micromamba_relpath}"
+wget -O - https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 
-# export MAMBA_ROOT_PREFIX=/some/prefix  # optional, defaults to ~/micromamba
+eval "$(./bin/micromamba shell hook -s posix)"
 
-eval "$(./${micromamba_relpath} shell hook -s posix)"
-
-# Linux/bash:
 ${micromamba_abspath} shell init -s bash -r ${home_abspath}/micromamba  # this writes to your .bashrc file
-# sourcing the bashrc file incorporates the changes into the running session.
-# better yet, restart your terminal! echo "eval \"\$(micromamba shell hook --shell=bash)\" && micromamba activate ${environment_name}" >> ${home_abspath}/.bashrc
 echo "alias mm=${micromamba_abspath}" >> "${home_abspath}/.bash_aliases"
 
 source ${home_abspath}/.bashrc
 
-# micromamba create -n env_name ${environment_name} -c conda-forge
 micromamba --version
 # THIS NEEDS TO WORK
 mm --version
 
-# COPY ${environment_config} /environment.repo.yml
 yq eval-all \
   '. as $item ireduce ({}; . *+ $item)' \
   "${home_abspath}/environment.common.yml" \
@@ -47,5 +39,4 @@ cat "${home_abspath}/environment.repo.yml"
 echo "Merged:"
 cat "${home_abspath}/environment.merged.yml"
 
-#  conda env create --file "/environment.merged.yml"
 micromamba env create --file "${home_abspath}/environment.merged.yml" 
