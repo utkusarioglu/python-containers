eca81746be2a668535e11cd1582e7008e119341f
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-15 08:19:16 +0000

    py New

--- src/home/scripts/bootstrap/conda/install-micromamba.sh
+++ src/home/scripts/bootstrap/conda/install-micromamba.sh
@@ -25,26 +25,26 @@ eval "$(/usr/bin/micromamba shell hook -s posix --rc-file ${home_abspath}/.bashr
 echo "alias mm=micromamba" >> "${home_abspath}/.bash_aliases"
 
 source ${home_abspath}/.bashrc
-# source ${home_abspath}/.bash_aliases
+source ${home_abspath}/.bash_aliases
 
 micromamba --version
 # THIS NEEDS TO WORK
-# mm --version
+mm --version
 
-yq eval-all \
-  '. as $item ireduce ({}; . *+ $item)' \
-  "${home_abspath}/environment.common.yml" \
-  "${home_abspath}/environment.repo.yml" \
-  > "${home_abspath}/environment.merged.yml"
+# yq eval-all \
+#   '. as $item ireduce ({}; . *+ $item)' \
+#   "${home_abspath}/environment.common.yml" \
+#   "${home_abspath}/environment.repo.yml" \
+#   > "${home_abspath}/environment.merged.yml"
 
-echo "Common:"
-cat "${home_abspath}/environment.common.yml"
+# echo "Common:"
+# cat "${home_abspath}/environment.common.yml"
 
-echo "Repo:"
-cat "${home_abspath}/environment.repo.yml"
+# echo "Repo:"
+# cat "${home_abspath}/environment.repo.yml"
 
-echo "Merged:"
-cat "${home_abspath}/environment.merged.yml"
+# echo "Merged:"
+# cat "${home_abspath}/environment.merged.yml"
 
 micromamba env create --file "${home_abspath}/environment.merged.yml" 
 
