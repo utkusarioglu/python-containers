612c1ba5a3b19e494b91204ea20b234f0ed166c5
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-21 17:11:21 +0000

    py New

---
+++ src/music/home/scripts/bootstrap/music/do-image-specific.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARGS=(
+  environment_name
+)
+. ${0%/*}/../linux/parse-args.sh
+
+mkdir -p /opt/conda/envs/${environment_name}/lib/alsa-lib
+ln -s /usr/lib/x86_64-linux-gnu/alsa-lib/libasound_module_conf_pulse.so \
+  /opt/conda/envs/${environment_name}/lib/alsa-lib/
+
+mkdir -p /opt/conda/envs/${environment_name}/share/soundfonts
+ln -s /usr/share/sounds/sf2/FluidR3_GM.sf2 \
+  /opt/conda/envs/${environment_name}/share/soundfonts/default.sf2
