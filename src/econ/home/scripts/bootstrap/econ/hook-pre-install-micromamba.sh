8a81972209eee678084a2945585090222b88dda8
Utku Sarioglu <utkusarioglu@hotmail.com>
2025-04-03 18:25:41 +0000

    Feat: Econ more tools 6

--- src/econ/home/scripts/bootstrap/econ/hook-pre-install-micromamba.sh
+++ src/econ/home/scripts/bootstrap/econ/hook-pre-install-micromamba.sh
@@ -8,6 +8,6 @@ talib_filename=talib.deb
 wget -O $talib_filename https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib_0.6.4_amd64.deb
 dpkg -i $talib_filename
 rm $talib_filename
-echo 'See if ta-lib has been installed here:'
-dpkg -l | grep talib
-echo "--"
+# echo 'See if ta-lib has been installed here:'
+# dpkg -l | grep talib
+# echo "--"
