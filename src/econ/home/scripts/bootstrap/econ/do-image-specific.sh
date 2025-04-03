71017989d1af03ff0247237fcba5bc4192172faa
Utku Sarioglu <utkusarioglu@hotmail.com>
2025-04-03 17:56:45 +0000

    Feat: Econ more tools 1

--- src/econ/home/scripts/bootstrap/econ/do-image-specific.sh
+++ src/econ/home/scripts/bootstrap/econ/do-image-specific.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "No image specific tasks"
+set -euxo pipefail
+bash --version
+
+echo 'Installing talib'
+talib_filename=talib.deb
+wget -O $talib_filename https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib_0.6.4_amd64.deb
+dpkg -i $talib_filename
+rm $talib_filename
+ta-lib --version
